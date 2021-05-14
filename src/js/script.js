3ac8a505b87cd82760fa1074165d554d04e30048
vlad <fordreej@gmail.com>
2021-05-15 00:44:07 +0300

    finished the mobile and desktop version of the prшьÑimary section

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 document.addEventListener('scroll', () => {
-    parallax(document.querySelectorAll('.main__parallax-item'))
+    parallax(document.querySelectorAll('.main__parallax-item'), document.querySelector('.main__peach-top'))
 });
 
-function parallax(collection) {
+function parallax(collection, additionalElement = 0) {
     let scrollPosition = window.pageYOffset;
 
     if (scrollPosition < 700) {
@@ -10,11 +10,15 @@ function parallax(collection) {
             img.style.transform = `translate3d(0px, 
             ${scrollPosition * img.getAttribute('data-speedMove') / 10}px, 0px)`;
         }
+
+        if(window.screen.width < 721) {
+            additionalElement.style.transform = `translate3d(0px, 
+            ${scrollPosition * additionalElement.getAttribute('data-speedMove') / 10}px, 0px)`;
+        }
     } else {
         document.onscroll = null;
     }
 }
-
 
 
 
