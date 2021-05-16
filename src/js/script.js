3545b425ee64f8a83401bf6def227dd5c01888e2
vlad <fordreej@gmail.com>
2021-05-16 12:36:33 +0300

    finished capabilities section

--- src/js/script.js
+++ src/js/script.js
@@ -11,15 +11,42 @@ function parallax(collection, additionalElement = 0) {
             ${scrollPosition * img.getAttribute('data-speedMove') / 10}px, 0px)`;
         }
 
-        if(window.screen.width < 721) {
+        if(window.screen.availWidth < 721) {
             additionalElement.style.transform = `translate3d(0px, 
             ${scrollPosition * additionalElement.getAttribute('data-speedMove') / 10}px, 0px)`;
         }
+
     } else {
         document.onscroll = null;
     }
 }
 
+if (window.screen.availWidth < 993) {
+    const nav = document.querySelector('.menu__nav'),
+        bg = document.querySelector('.menu__bg');
 
+    document.querySelector('.main__hamburger').addEventListener('click', (e) => {
+        bg.style.right = '0';
+        bg.style.opacity = '1';
 
+        nav.style.left = '0';
+        nav.style.opacity = '1';
+    });
+
+    document.querySelector('.menu__close').addEventListener('click', (e) => {
+        bg.style.right = '-100%';
+        bg.style.opacity = '0';
+
+        nav.style.left = '-100%';
+        nav.style.opacity = '0';
+    });
+    document.querySelector('.menu__bg').addEventListener('click', (e) => {
+        bg.style.right = '-100%';
+        bg.style.opacity = '0';
+
+        nav.style.left = '-100%';
+        nav.style.opacity = '0';
+    });
+
+}
 
